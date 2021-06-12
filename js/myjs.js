8d3acb89fd2e30d1d5692cb76fc6d3880993c1be
TongjiHelper <Tongji-Helper@protonmail.com>
2021-06-12 19:06:25 +0800

    myjs.js: remove unused functions

--- js/myjs.js
+++ js/myjs.js
@@ -17,37 +17,6 @@
  * Copyright (C) 2021 TongjiHelper
  */
 
-function logout() {
-  let exp = new Date();
-  exp.setTime(1);
-  document.cookie="PHPSESSID=x;expires="+exp.toGMTString();
-  location.reload();
-}
-function lock(tip) {
-  $('#waiting>h1').html(tip).css('margin-top',$(window).height()/2-60);
-  $('#waiting').css('display','block');
-}
-function unlock() {
-  $('#waiting').css('display','none');
-}
-function redtip (txt,time) {
-  $('#login_error').css({'top':$(window).height()/2,'display':'block'}).html(txt).hide().slideDown(400).delay(time).slideToggle(400);
-}
-function bluetip (txt,time) {
-  $('#blue').css({'top':$(window).height()/2,'display':'block'}).html(txt).hide().slideDown(400).delay(time).slideToggle(400);
-}
-function getGet(name){
-  let reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-  let r = window.location.search.substr(1).match(reg);
-  if(r!=null) return unescape(r[2]);
-  return null;
-}
-function getCookie(name){
-  let reg = new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-  let r = document.cookie.match(reg);
-  if(r!=null) return unescape(r[2]);
-  return null;
-}
 async function getStorage(key,default_value){
   return new Promise((resolve,reject)=>{
     chrome.storage.local.get([key],items=> {
@@ -56,52 +25,33 @@ async function getStorage(key,default_value){
     });
   });
 }
-async function setStorage(key,value){
-  return new Promise((resolve,reject)=>{
-    let pair={};
-    pair[key]=value;
-    chrome.storage.local.set(pair,()=> {
-      resolve();
-    });
-  });
+
+function insertBS () {
+  $('head').append('<link href="'+chrome.runtime.getURL('css/bootstrap.min.css')+'" rel="stylesheet"><link href="'+chrome.runtime.getURL('css/mystyle.css')+'" rel="stylesheet"><script src="'+chrome.runtime.getURL('js/jquery-3.1.1.min.js')+'"></script>')
+  setTimeout(function(){$('head').append('<script src="'+chrome.runtime.getURL('js/bootstrap.min.js')+'"></script>')},100)
 }
-function myalert(tip,onhidden) {
-  $('#myalert').find('p').html(tip);
-  $('#myalert').modal('show');
-  $('#myalert').on('hidden.bs.modal', onhidden);
-}
-function success(tip,onhidden) {
-  $('#success').find('p').html(tip);
-  $('#success').modal('show');
-  $('#success').on('hidden.bs.modal', onhidden);
-}
-function insertBS(){
-  $('head').append('<link href="'+chrome.runtime.getURL('css/bootstrap.min.css')+'" rel="stylesheet"><link href="'+chrome.runtime.getURL('css/mystyle.css')+'" rel="stylesheet"><script src="'+chrome.runtime.getURL('js/jquery-3.1.1.min.js')+'"></script>');
-  setTimeout(function(){$('head').append('<script src="'+chrome.runtime.getURL('js/bootstrap.min.js')+'"></script>')},100);
-}
+
 function myeval(evalstr) {
-  $('body').append('<script>'+evalstr+'</script>');
+  $('body').append('<script>'+evalstr+'</script>')
 }
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-Date.prototype.format = function(fmt) {
+
+Date.prototype.format = function (fmt) {
   let o = {
-    "M+" : this.getMonth()+1,
-    "d+" : this.getDate(), //日
-    "H+": this.getHours(), //小时
-    "m+": this.getMinutes(), //分
-    "s+": this.getSeconds(), //秒
-    "S": this.getMilliseconds() //毫秒
-  };
-  if(/(y+)/.test(fmt)) {
-    fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    'M+' : this.getMonth() + 1,
+    'd+' : this.getDate(),
+    'H+' : this.getHours(),
+    'm+' : this.getMinutes(),
+    's+' : this.getSeconds(),
+    'S' : this.getMilliseconds()
   }
-  for(let k in o) {
-    if(new RegExp("("+ k +")").test(fmt)){
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+  for (let k in o) {
+    if (new RegExp('('+ k +')').test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (('00'+ o[k]).substr((''+ o[k]).length)))
     }
   }
-  return fmt;
+  return fmt
 }
-let server="https://1.tongji.edu.cn";
+let server='https://1.tongji.edu.cn'
